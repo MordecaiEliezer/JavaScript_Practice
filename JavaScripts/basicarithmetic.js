8ec2b107682a13aadc34ea00520c8a08f385351f
Eliezer <136172001+MordecaiEliezer@users.noreply.github.com>
2023-11-30 18:02:26 +0800

    Add files via upload

---
+++ JavaScripts/basicarithmetic.js
@@ -0,0 +1,13 @@
+/*console.log(3+4);
+console.log(3-4);
+console.log(3*4);
+console.log(3/4);
+*/
+
+let a = 4;
+let b = 5;
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log(a-b);
---
+++ JavaScripts/simplecal.js
@@ -0,0 +1,11 @@
+let a = 4;
+let b = 10;
+//this will ony accept + and -
+//Use boolean + = true and - = false
+let bool = true;
+
+if(bool == true){
+    console.log(a+b);
+}else if(bool == false){
+    console.log(a-b);
+}
